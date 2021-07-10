1a936816aa8c9afe0f466f9e4d0756f87fa6c2a9
aereal <aereal@aereal.org>
2021-07-11 01:34:30 +0900

    refactor: only ButtonProps are variable

--- src/components/OpenReservationButton.tsx
+++ src/components/OpenReservationButton.tsx
@@ -1,14 +1,9 @@
-import { Button, makeStyles } from "@material-ui/core";
+import { Button, ButtonProps, makeStyles } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import CheckIcon from "@material-ui/icons/Check";
 import React, { FC } from "react";
 import { ReservationSchedule } from "../types";
 import { use履修済み教科 } from "../履修済み教科";
-
-export interface OpenReservationButtonProps {
-  readonly onClick: (schedule: ReservationSchedule) => void;
-  readonly schedule: ReservationSchedule;
-}
 
 const useStyles = makeStyles((theme) => ({
   reserved: {
@@ -17,39 +12,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const AlreadyReservedButton: FC<OpenReservationButtonProps> = ({
-  onClick,
+const renderButtonProps = (args: {
+  readonly 履修済み: boolean;
+  readonly classes: ReturnType<typeof useStyles>;
+}): ButtonProps =>
+  args.履修済み ? { className: args.classes.reserved } : { color: "primary" };
+
+const ButtonIcon: FC<{ readonly 履修済み: boolean }> = ({ 履修済み }) =>
+  履修済み ? <CheckIcon /> : <AddIcon />;
+
+export interface OpenReservationButtonProps {
+  readonly onClick: (schedule: ReservationSchedule) => void;
+  readonly schedule: ReservationSchedule;
+}
+
+export const OpenReservationButton: FC<OpenReservationButtonProps> = ({
   schedule,
+  onClick,
 }) => {
+  const 履修済み教科 = use履修済み教科();
   const classes = useStyles();
+  const 履修済み = 履修済み教科.has(schedule.教科);
   return (
-    <Button onClick={onClick.bind(null, schedule)} className={classes.reserved}>
-      <CheckIcon />
+    <Button
+      onClick={onClick.bind(null, schedule)}
+      {...renderButtonProps({ 履修済み, classes })}
+    >
+      <ButtonIcon 履修済み={履修済み} />
       {schedule.教科}
     </Button>
-  );
-};
-
-const ReserveButton: FC<OpenReservationButtonProps> = ({
-  onClick,
-  schedule,
-}) => (
-  <Button
-    onClick={onClick.bind(null, schedule)}
-    color="primary" // TODO: 教科ごとに色をつける
-  >
-    <AddIcon />
-    {schedule.教科}
-  </Button>
-);
-
-export const OpenReservationButton: FC<OpenReservationButtonProps> = (
-  props
-) => {
-  const 履修済み教科 = use履修済み教科();
-  return 履修済み教科.has(props.schedule.教科) ? (
-    <AlreadyReservedButton {...props} />
-  ) : (
-    <ReserveButton {...props} />
   );
 };
