0d805e4a4b2e04f14a37b7f52f3807c9d5996c76
aereal <aereal@aereal.org>
2021-07-12 02:38:38 +0900

    fix: correct subject phases

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { RouteProvider, useRoute } from "./router";
 import { CustomThemeProvider } from "./theme";
 import { 教科 } from "./types";
 
-const 一覧: 教科[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const 一覧: 教科[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 const Page: FC = () => {
   const [progressRepo, setProgressRepo] = useState(
--- src/types.ts
+++ src/types.ts
@@ -9,9 +9,9 @@ export interface ReservationSchedule extends Schedule {
 
 export const 時限一覧 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as const;
 
-export const 第一段階教科一覧 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as const;
+export const 第一段階教科一覧 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] as const;
 export const 第二段階教科一覧 = [
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
 ] as const;
 export const 教科一覧 = [...第一段階教科一覧, ...第二段階教科一覧] as const;
 
@@ -21,4 +21,5 @@ export type 第二段階教科 = typeof 第二段階教科一覧[number];
 
 export type 教科 = 第一段階教科 | 第二段階教科;
 
-export const is第一段階 = (x: 教科): x is 第一段階教科 => x >= 1 && x <= 10;
+export const is第一段階 = (x: 教科): x is 第一段階教科 =>
+  x >= 第一段階教科一覧[0] && x < 第二段階教科一覧[0];
