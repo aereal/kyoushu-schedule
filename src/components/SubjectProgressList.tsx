066a8070fe805da70709f6fdebf771a617da83c4
aereal <aereal@aereal.org>
2021-07-31 12:54:40 +0900

    feat: add schedule links to progress list

--- src/components/SubjectProgressList.tsx
+++ src/components/SubjectProgressList.tsx
@@ -1,30 +1,21 @@
 import {
   Checkbox,
   CheckboxProps,
-  Collapse,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
 } from "@material-ui/core";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Alert } from "@material-ui/lab";
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { useStudyProgressRepository } from "../contexts/study-progress-repo";
 import { formatShortDate } from "../date";
-import { getValue, maybe } from "../maybe";
+import { maybe } from "../maybe";
 import { periodTimeRange } from "../period";
 import { StudyProgress } from "../repositories/study-progress";
+import { routes } from "../router";
 import { formatTerm } from "../term";
 import { 教科 } from "../types";
 import { DateTime } from "./DateTime";
-
-const subjectNotes = new Map<教科, string>([
-  [11, "受講していなくとも卒業テストを受験可 (技能14, 15の後に実施)"],
-  [25, "技能教習10 (自主経路設定教習) の前に受講すること"],
-  [26, "技能教習16, 17 (高速道路教習) の前に受講すること"],
-]);
 
 const renderProps = (
   progress: StudyProgress
@@ -35,59 +26,51 @@ const renderProps = (
     ? { indeterminate: true }
     : {};
 
-const noop = () => {};
-
 interface ProgressListItemProps {
   readonly progress: StudyProgress;
 }
 
 const ProgressListItem: FC<ProgressListItemProps> = ({ progress }) => {
-  const [open, setOpen] = useState(false);
-  const note = getValue(subjectNotes, progress.subject);
-  const [handleClick, isButton] = note.fold<[() => void, false | undefined]>(
-    () => [
-      () => {
-        setOpen((prev) => !prev);
-      },
-      undefined,
-    ],
-    () => [noop, false]
+  const C = maybe(progress.taken ?? progress.reservation).fold<FC>(
+    (schedule) =>
+      ({ children }) =>
+        (
+          <ListItem
+            button
+            children={children}
+            component="a"
+            {...routes.schedule({
+              schedule: { ...schedule, 教科: progress.subject },
+            }).link}
+          />
+        ),
+    () =>
+      ({ children }) =>
+        <ListItem children={children} />
   );
   return (
-    <>
-      <ListItem button={isButton} onClick={handleClick}>
-        <ListItemIcon>
-          <Checkbox
-            edge="start"
-            tabIndex={-1}
-            disableRipple
-            {...renderProps(progress)}
-          />
-        </ListItemIcon>
-        <ListItemText>
-          {progress.subject}{" "}
-          {maybe(progress.reservation).fold(
-            ({ date, 時限 }) => (
-              <>
-                <DateTime dateTime={date}>{formatShortDate(date)}</DateTime>{" "}
-                {時限}時限目 {formatTerm(periodTimeRange[時限])}
-              </>
-            ),
-            () => null
-          )}
-        </ListItemText>
-        {note
-          .map(() => (open ? <ExpandLessIcon /> : <ExpandMoreIcon />))
-          .unwrap()}
-      </ListItem>
-      {note
-        .map((content) => (
-          <Collapse in={open} unmountOnExit timeout="auto">
-            <Alert severity="info">{content}</Alert>
-          </Collapse>
-        ))
-        .unwrap()}
-    </>
+    <C>
+      <ListItemIcon>
+        <Checkbox
+          edge="start"
+          tabIndex={-1}
+          disableRipple
+          {...renderProps(progress)}
+        />
+      </ListItemIcon>
+      <ListItemText>
+        {progress.subject}{" "}
+        {maybe(progress.reservation).fold(
+          ({ date, 時限 }) => (
+            <>
+              <DateTime dateTime={date}>{formatShortDate(date)}</DateTime>{" "}
+              {時限}時限目 {formatTerm(periodTimeRange[時限])}
+            </>
+          ),
+          () => null
+        )}
+      </ListItemText>
+    </C>
   );
 };
 
